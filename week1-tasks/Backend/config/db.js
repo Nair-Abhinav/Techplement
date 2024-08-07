4abeaa0fa961e759ec7880a20e8b60638bf7fff5
Abhinav <abhinavnair7404@gmail.com>
2024-08-07 20:25:08 +0530

    database error testing

--- week1-tasks/Backend/config/db.js
+++ week1-tasks/Backend/config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+// const db = process.env.MONGO_URI || 'mongodb+srv://abhinavnair7404:Abc1234@cluster0.3dau0gk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(db);
+//     console.log('MongoDB connected');
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1);
+//   }
+// };
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
+// module.exports = connectDB;
--- week1-tasks/Backend/db/index.db.js
+++ week1-tasks/Backend/db/index.db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const db_name = require('../constants/constant.values')
+const db = process.env.MONGO_URI || 'mongodb+srv://abhinavnair7404:Abc1234@cluster0.3dau0gk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const connection_DB = async ()=>{
     try{
-        const con = await mongoose.connect(`${process.env.MONGO_URI}/${db_name}`);
+        const con = await mongoose.connect(db);
         console.log(`Database connected successfully DB HOST:${con.connection.host}`);
     }catch(e){
         console.log("Mongo Connection Failed");
