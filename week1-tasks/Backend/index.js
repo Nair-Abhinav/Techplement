5e3813b9190c0ca542ed097111d43f2a732ba39b
Nair-Abhinav <143010704+Nair-Abhinav@users.noreply.github.com>
2024-08-14 21:19:41 +0530

    Update index.js

--- week1-tasks/Backend/index.js
+++ week1-tasks/Backend/index.js
@@ -6,12 +6,12 @@ const transactionRoutes = require('./routes/transaction');
 const cors = require('cors');
 const connection_DB = require('./db/index.db');
 dotenv.config();
-app.use(cors());  
-// {
-//   origin: ['https://techplement-task-frontend.vercel.app'],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true
-// }
+app.use(cors({
+  origin: ['https://techplement-task-frontend.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));  
+
 app.use(express.json());
 connection_DB();
 
