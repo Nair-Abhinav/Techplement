1cab466acd924d2d0dd21558ce290ad06ec31637
Nair-Abhinav <143010704+Nair-Abhinav@users.noreply.github.com>
2024-08-14 21:54:59 +0530

    Update index.js

--- week1-tasks/Backend/index.js
+++ week1-tasks/Backend/index.js
@@ -9,7 +9,13 @@ dotenv.config();
 app.use(cors());  
 app.use(express.json());
 connection_DB();
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://techplement-task-frontend.vercel.app'); // Set the exact origin
+    res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE'); // Optional: Allow specific methods
+    res.header('Access-Control-Allow-Headers', 'Content-Type'); // Optional: Allow specific headers
+    next();
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/transactions', transactionRoutes);
 const PORT = process.env.PORT;
