4eb4ab72fafdf13434207bbe61c93c3c7171cb6f
Nair-Abhinav <143010704+Nair-Abhinav@users.noreply.github.com>
2024-08-14 20:50:14 +0530

    Update vite.config.js

--- week1-tasks/Frontend/vite.config.js
+++ week1-tasks/Frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://techplement-eosin.vercel.app/',
-        changeOrigin: true,
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://techplement-eosin.vercel.app/',
+  //       changeOrigin: true,
+  //     }
+  //   }
+  // }
 })
