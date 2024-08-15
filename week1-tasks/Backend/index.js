f73e83a188a30311de871fcdc4a2fbd9bed26656
Nair-Abhinav <143010704+Nair-Abhinav@users.noreply.github.com>
2024-08-15 11:31:53 +0530

    Update index.js

--- week1-tasks/Backend/index.js
+++ week1-tasks/Backend/index.js
@@ -6,24 +6,24 @@ const transactionRoutes = require('./routes/transaction');
 const cors = require('cors');
 const connection_DB = require('./db/index.db');
 dotenv.config();
-app.use(cors());  
 app.use(express.json());
-connection_DB();
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    'https://techplement-task-frontend.vercel.app',
-    'https://techplement-eta.vercel.app',
-    'https://techplement-backend-eight.vercel.app'
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+app.use(cors());  
+
+// app.use((req, res, next) => {
+//   const allowedOrigins = [
+//     'https://techplement-task-frontend.vercel.app',
+//     'https://techplement-eta.vercel.app',
+//     'https://techplement-backend-eight.vercel.app'
+//   ];
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 app.use('/api/auth', authRoutes);
 app.use('/api/transactions', transactionRoutes);
